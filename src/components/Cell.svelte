08294a1ad38534e3979d7f222d10485d4b7473e9
emanuel-flowih <emanuel@flowih.com>
2023-11-13 17:22:46 -0300

    Create Grid and Cell components

---
+++ src/components/Cell.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    export let letter: string | undefined;
+    export let isBlack: boolean = false;
+</script>
+
+<div class="cell" class:is-black={isBlack}>
+    {#if !isBlack}
+        <input type="text" value={letter} />
+    {/if}
+</div>
+
+<style>
+    .cell {
+        width: 30px;
+        height: 30px;
+        border: 1px solid black;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .is-black {
+        background-color: black;
+    }
+
+    input {
+        width: 100%;
+        height: 100%;
+        font-size: 20px;
+        text-align: center;
+        border: none;
+        background-color: transparent;
+    }
+</style>
---
+++ src/components/Grid.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import Cell from './Cell.svelte';
+
+    export let rows:number;
+    export let cols:number;
+    export let words:string[][];
+
+    function isBlackCell(row: number, col: number) {
+        if (words[row][col] === '') {
+            return true;
+        }
+    }
+
+    function getLetter(row, col) {
+        return words[row][col];
+    }
+</script>
+
+<div class="grid">
+    {#each Array(rows) as _, row}
+        <div class="row">
+            {#each Array(cols) as _, col}
+                {#if isBlackCell(row, col)}
+                    <Cell isBlack={true} />
+                {:else}
+                    <Cell letter={getLetter(row, col)} />
+                {/if}
+            {/each}
+        </div>
+    {/each}
+</div>
+
+<style>
+    .grid {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .row {
+        display: flex;
+    }
+</style>
