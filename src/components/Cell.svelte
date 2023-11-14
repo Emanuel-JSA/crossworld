51ecc2ac322cb3879a0fa6b58462335c8c849a19
emanuel-flowih <emanuel@flowih.com>
2023-11-14 14:44:14 -0300

    Change components for the new logic and store

--- src/components/Cell.svelte
+++ src/components/Cell.svelte
@@ -1,34 +1,60 @@
 <script lang="ts">
-    export let letter: string | undefined;
-    export let isBlack: boolean = false;
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	export let isBlack: boolean = false;
+	export let isBlocked: boolean = false;
+	export let rowPosition: number;
+	export let colPosition: number;
+	export let value = '';
+
+	function handleInput(event: Event) {
+		const target = event.target as HTMLInputElement; // Cast event.target to HTMLInputElement
+		if (target) {
+			// Check if target is not null
+			value = target.value;
+			dispatch('input', { value: value, rowPosition: rowPosition, colPosition: colPosition });
+		}
+	}
 </script>
 
 <div class="cell" class:is-black={isBlack}>
-    {#if !isBlack}
-        <input type="text" value={letter} />
-    {/if}
+	{#if !isBlack}
+		<input
+			type="text"
+			bind:value
+			maxlength="1"
+			on:input={handleInput}
+			class:is-blocked={isBlocked}
+            disabled={isBlocked}
+		/>
+	{/if}
 </div>
 
 <style>
-    .cell {
-        width: 30px;
-        height: 30px;
-        border: 1px solid black;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+	.cell {
+		width: 30px;
+		height: 30px;
+		border: 1px solid black;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
 
-    .is-black {
-        background-color: black;
-    }
+	.is-black {
+		background-color: black;
+	}
 
-    input {
-        width: 100%;
-        height: 100%;
-        font-size: 20px;
-        text-align: center;
-        border: none;
-        background-color: transparent;
-    }
+	.is-blocked {
+		background-color: orange;
+	}
+
+	input {
+		width: 100%;
+		height: 100%;
+		font-size: 20px;
+		text-align: center;
+		border: none;
+		background-color: transparent;
+	}
 </style>
--- src/components/Grid.svelte
+++ src/components/Grid.svelte
@@ -1,43 +1,86 @@
 <script lang="ts">
-    import Cell from './Cell.svelte';
+	import { checkWord } from '../utils/wordCheck';
+	import type { GameState } from '../store';
+	import { gameStore } from '../store';
+	import Cell from './Cell.svelte';
 
-    export let words:string[][];
-    let rows = words.length;
-    let cols = words[0].length;
+	let rows: number;
+	let cols: number;
 
-    function isBlackCell(row: number, col: number) {
-        if (words[row][col] === '') {
-            return true;
-        }
-    }
+	// Accessing store values
+	let state: GameState;
+	gameStore.subscribe((value: GameState) => {
+		state = value;
+		rows = state.words.length;
+		cols = state.words[0].length;
+	});
 
-    function getLetter(row: number, col: number) {
-        return words[row][col];
-    }
+	function isBlackCell(row: number, col: number) {
+		if (state.words[row][col] === '') {
+			return true;
+		}
+	}
+
+	function handleCellInput(event: CustomEvent) {
+		let value = event.detail.value;
+		let rowPosition = event.detail.rowPosition;
+		let colPosition = event.detail.colPosition;
+		let found: boolean = false;
+
+		updateBlankWords(rowPosition, colPosition, value);
+
+		state.wordsToFind.forEach((word) => {
+			found = checkWord(state.blankWords, word);
+		});
+	}
+
+	
+	function updateBlankWords(rowPosition: number, colPosition: number, value: string) {
+		state.blankWords[rowPosition][colPosition] = value;
+	}
+
+	// function checkWin() {
+	// 	for (let i = 0; i < rows; i++) {
+	// 		for (let j = 0; j < cols; j++) {
+	// 			if (words[i][j] !== blankWords[i][j]) {
+	//                 return;
+	// 			}
+	// 		}
+	// 	}
+
+	//     console.log('You win!');
+	// }
 </script>
 
 <div class="grid">
-    {#each Array(rows) as _, row}
-        <div class="row">
-            {#each Array(cols) as _, col}
-                {#if isBlackCell(row, col)}
-                    <Cell isBlack={true} />
-                {:else}
-                    <Cell letter={getLetter(row, col)} />
-                {/if}
-            {/each}
-        </div>
-    {/each}
+	{#each Array(rows) as _, row}
+		<div class="row">
+			{#each Array(cols) as _, col}
+				{#if isBlackCell(row, col)}
+					<Cell isBlack={true} rowPosition={row} colPosition={col} />
+				{:else if $gameStore.matchWords[row][col]}
+					<Cell
+						isBlocked={true}
+						value={state.words[row][col]}
+						rowPosition={row}
+						colPosition={col}
+					/>
+				{:else}
+					<Cell on:input={handleCellInput} rowPosition={row} colPosition={col} />
+				{/if}
+			{/each}
+		</div>
+	{/each}
 </div>
 
 <style>
-    .grid {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
+	.grid {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 
-    .row {
-        display: flex;
-    }
+	.row {
+		display: flex;
+	}
 </style>
