35aab98c4eb5fcad886478d66a98acf6f1336015
emanuel-flowih <emanuel@flowih.com>
2023-11-18 17:39:21 -0300

    Create admin route

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Grid from '../../components/Grid.svelte';
+	import GridSizeSelect from '../../components/GridSizeSelect.svelte';
+	import { gameStore } from '../../store';
+</script>
+
+<h1>ADM</h1>
+
+<GridSizeSelect />
+<Grid rows={$gameStore.words.length} cols={$gameStore.words[0].length} />
