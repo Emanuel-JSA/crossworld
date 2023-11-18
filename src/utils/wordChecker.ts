a49265cf22382f0ae9f749f4d593b1ddf518ab36
emanuel-flowih <emanuel@flowih.com>
2023-11-18 17:36:23 -0300

    Refactor wordChecker to a ts object

---
+++ src/utils/wordChecker.ts
@@ -0,0 +1,69 @@
+import { get } from 'svelte/store';
+import { gameStore } from '../store';
+
+const WordChecker = {
+    checkWord(grid: string[][], word: string): boolean {
+        const directions = [
+            { x: 0, y: 1 }, // horizontal
+            { x: 1, y: 0 }, // vertical
+        ];
+
+        // Verifica cada posição da grade.
+        for (let row = 0; row < grid.length; row++) {
+            for (let col = 0; col < grid[row].length; col++) {
+                // Verifica em cada direção.
+                for (const dir of directions) {
+                    if (this.checkWordInDirection(grid, word, row, col, dir)) {
+                        console.log('Found word: ' + word);
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    },
+
+    checkWordInDirection(
+        grid: string[][],
+        word: string,
+        row: number,
+        col: number,
+        dir: { x: number; y: number }
+    ): boolean {
+        const storeValue = get(gameStore);
+
+        // Create a copy of matchWords
+        const newMatchWords = storeValue.matchWords.map(row => [...row]);
+
+        const wordLength = word.length;
+
+        // Check if the word fits in the direction.
+        const endRow = row + dir.x * (wordLength - 1);
+        const endCol = col + dir.y * (wordLength - 1);
+        if (endRow < 0 || endRow >= grid.length || endCol < 0 || endCol >= grid[row].length) {
+            return false;
+        }
+
+        // Check if the word is found in the direction.
+        for (let i = 0; i < wordLength; i++) {
+            const currentRow = row + dir.x * i;
+            const currentCol = col + dir.y * i;
+            if (grid[currentRow][currentCol] !== word[i]) {
+                return false;
+            }
+            newMatchWords[currentRow][currentCol] = true;
+        }
+
+        // Update the game store with the found word and the new matchWords.
+        gameStore.set({
+            ...storeValue,
+            foundWords: [...storeValue.foundWords, { word, row, col }],
+            matchWords: newMatchWords
+        });
+
+        return true;
+    }
+};
+
+export default WordChecker;
