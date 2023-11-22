cefeed64f6ed2cd8a84194b2284d64ed334a0fd9
emanuel-flowih <emanuel@flowih.com>
2023-11-22 15:09:44 -0300

    Use derived stores

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
 	import Grid from '../../components/Grid.svelte';
 	import GridSizeSelect from '../../components/GridSizeSelect.svelte';
-	import { gameStore } from '../../store';
+	import { gameStore, gameSize } from '../../store';
+	import GameServices from '../../services/gameServices';
+
+	function SubtmitGame() {
+		GameServices.submitGame($gameStore.words);
+	}
 </script>
 
 <h1>ADM</h1>
 
 <GridSizeSelect />
-<Grid rows={$gameStore.words.length} cols={$gameStore.words[0].length} />
+<Grid rows={$gameSize.rows} cols={$gameSize.cols} />
+<button on:click={SubtmitGame}>submit game</button>
