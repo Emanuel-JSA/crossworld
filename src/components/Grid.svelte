25a0f57e7ba0b293f7d85929a90b42dd7b456e9d
emanuel-flowih <emanuel@flowih.com>
2023-11-22 15:09:29 -0300

    Handle delete grid moviment

--- src/components/Grid.svelte
+++ src/components/Grid.svelte
@@ -1,29 +1,35 @@
 <script lang="ts">
 	import Cell from './Cell.svelte';
 	import { userStore, gameStore, Direction } from '../store';
-	import type { GameState } from '../store';
 	export let rows: number;
 	export let cols: number;
 
-	$: checkFocus =  function(row: number, col: number) {
+	$: checkFocus = function (row: number, col: number) {
 		if ($userStore.moveDirection == Direction.Horizontal) {
 			return $userStore.currentPosition.row == row;
 		} else {
 			return $userStore.currentPosition.col == col;
 		}
-	}
+	};
+
+	function handleCellInput(event: CustomEvent, row: number, col: number) {
+		if (event.detail.value == undefined) {
+			eraseValue(row, col);
+			return;
+		}
 	
-	function handleCellInput(event: CustomEvent) {
-		let value = event.detail.value;
-		let rowPosition = event.detail.rowPosition;
-		let colPosition = event.detail.colPosition;
-		
-		updateGameStore(rowPosition, colPosition, value);
+		const value = event.detail.value.replace(/[^A-Za-z]/g, '').toUpperCase();
+
+		if (value.length > 1 || value == '' || value == null) return;
+
+		updateGameStore(row, col, value);
+		console.log($gameStore.words);
 	}
 
-	function updateGameStore(rowPosition: number, colPosition: number, value: string) {
-		const words = $gameStore.blankWords.map(row => [...row]);
-		words[rowPosition][colPosition] = value;
+	function updateGameStore(row: number, col: number, value: string) {
+		const words = $gameStore.words;
+		// allow only letters
+		words[row][col] = value;
 		gameStore.update((value) => {
 			return {
 				...value,
@@ -32,18 +38,28 @@
 		});
 	}
 
+	function eraseValue(row: number, col: number) {
+		const words = $gameStore.words;
+		words[row][col] = '';
+		gameStore.update((value) => {
+			return {
+				...value,
+				words: words
+			};
+		});
+	}
 </script>
 
 <div class="grid">
 	{#each Array(rows) as _, row}
 		<div class="row">
 			{#each Array(cols) as _, col}
-				<Cell 
-					rowPosition={row} 
-					colPosition={col} 
-					isOnFocus={checkFocus(row, col)} 
-					on:input={handleCellInput}
-					/>
+				<Cell
+					rowPosition={row}
+					colPosition={col}
+					isOnFocus={checkFocus(row, col)}
+					on:input={(event) => handleCellInput(event, row, col)}
+				/>
 			{/each}
 		</div>
 	{/each}
